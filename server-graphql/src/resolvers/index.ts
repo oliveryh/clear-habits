83cb92a2d12fc0e6575b59a9f46188fd02cfcbd9
oliveryh <oyerburyhodgson@gmail.com>
2021-10-24 17:25:04 +0100

    :fire: Remove server-postgraphile and server legacy code

--- server-graphql/src/resolvers/index.ts
+++
@@ -1,8 +0,0 @@
-import userResolvers from './user';
-import categoryResolvers from './category';
-import projectResolvers from './project';
-import taskResolvers from './task';
-import entryResolvers from './entry'
-import statsResolvers from './stats'
-
-export default [userResolvers, categoryResolvers, projectResolvers, taskResolvers, entryResolvers, statsResolvers];

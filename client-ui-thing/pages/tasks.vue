bf4932e0431f41dff93b3d33d9c91e80befcdbb6
Oliver Yerbury-Hodgson <oyerburyhodgson@gmail.com>
2024-01-15 19:02:43 +0000

    :sparkles: feat: filter entries (#238)

--- client-ui-thing/pages/tasks.vue
+++ client-ui-thing/pages/tasks.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="flex grid items-center justify-center gap-6 pt-4">
-    <template v-for="entry in result?.entries">
-      <UiCard
-        class="w-[360px] max-w-sm"
-        :title="entry.task.description"
-        :description="entry.description"
-      >
+    <div class="justify-right flex w-full">
+      <div class="flex items-center space-x-2">
+        <UiSwitch id="show-completed" v-model:checked="showCompleted" />
+        <UiLabel for="show-completed">Show Completed</UiLabel>
+      </div>
+    </div>
+    <template v-for="entry in filteredEntries">
+      <UiCard class="w-[360px] max-w-sm">
+        <template #title>
+          <div class="mb-2">
+            <span
+              class="rounded-md px-2 py-1 text-sm"
+              :style="{ backgroundColor: entry.task.project.category.color }"
+            >
+              {{ entry.task.project.description.toUpperCase() }}
+            </span>
+          </div>
+          <UiCardTitle>{{ entry.task.description }}</UiCardTitle>
+        </template>
+        <template #description>
+          <UiCardDescription v-if="entry.description">{{ entry.description }}</UiCardDescription>
+        </template>
       </UiCard>
     </template>
   </div>
@@ -15,7 +31,20 @@
   import { Q_ENTRY } from "@/graphql/queries"
 
   const today = new Date()
-  const { result } = useQuery(Q_ENTRY, {
+
+  const allEntries = ref([])
+
+  const { onResult } = useQuery(Q_ENTRY, {
     datesIn: [today.toISOString().slice(0, 10)],
+  })
+  onResult((queryResult) => {
+    if (queryResult.loading) return
+    allEntries.value = queryResult.data.entries
+  })
+
+  const showCompleted = ref(false)
+  const filteredEntries = computed(() => {
+    if (showCompleted.value) return allEntries.value
+    return allEntries.value.filter((entry) => entry.complete)
   })
 </script>
